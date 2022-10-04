12980db040d97b1979027c9e567d58bcfca78d50
kankit08 <kankit08@gmail.com>
2022-10-04 15:21:22 +0530

    fixing few bugs & finilizing the script

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
-
+const Razorpay = require("razorpay");
 const express = require("express");
 const app = express();
-
+app.use(express.static("./public"));
 app.use(express.json());
 
 const { PORT } = process.env;
